ce4a2d11af0983fefedb83bd35322a3cdc461b9a
Abd ElRahman Ashraf <33667787+AbdelrahmanAshraf98@users.noreply.github.com>
2022-06-29 20:14:03 +0200

    add api constants add network service

---
+++ lib/common/api_constants.dart
@@ -0,0 +1,9 @@
+
+class ApiConstants{
+  static const String API_KEY='41ddedb44b521fc7a061dc3e45facf2f';
+  static const String BASE_URL='https://api.themoviedb.org/3/';
+  static const String PERSON_PREFIX='/person';
+  static const String POPULAR_PREFIX='/popular';
+
+
+}
---
+++ lib/services/network_service.dart
@@ -0,0 +1,10 @@
+import 'package:http/http.dart' as http;
+import 'package:movies_app/common/api_constants.dart';
+class NetworkService{
+
+  Future<http.Response> get(baseUrl,header){
+    baseUrl += '?api_key=${ApiConstants.API_KEY}&language=en-US';
+    return http.get(Uri.parse(baseUrl),headers: header);
+  }
+
+}
